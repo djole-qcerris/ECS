5f6eb9dd3336bfc649a8a1b3e49f727e49829eb9
djole-qcerris <djordje.raznatovic@qcerris.com>
2021-11-16 16:23:06 +0100

    Setting environment variable ccsAdress and importing ccs.jar

--- src/main/java/carProject/ECS/service/ECSService.java
+++ src/main/java/carProject/ECS/service/ECSService.java
@@ -1,21 +1,26 @@
 package carProject.ECS.service;
 
+import carProject.CCS.models.UserCredentials;
 import com.example.Jobs.model.Job;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ECSService {
 
+    @Value("${ccs.server.address}")
+    private String ccsAddress;
+
     @KafkaListener(topics = "kafka_job")
     public void getFromKafka(Job job){
 
-        final String uri = "http://localhost:8080/api/" + job.getName_of_site();
+        final String uri = ccsAddress + job.getName_of_site();
         System.out.println(uri);
-      //  RestTemplate restTemplate = new RestTemplate();
-      //  ThirdParty tcp = restTemplate.getForObject(uri, ThirdParty.class);
+        RestTemplate restTemplate = new RestTemplate();
+        UserCredentials uc = restTemplate.getForObject(uri, UserCredentials.class);
 
-       // System.out.println(tcp);
     }
 
 }
