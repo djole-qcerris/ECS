a00fc20d79351dfa288935cfd13d79dc71aaf709
Bojan Galic <bojan.galic@qcerris.com>
2021-11-25 17:31:06 +0100

    Added sending cars to kafka

--- src/main/java/carProject/ECS/service/ECSService.java
+++ src/main/java/carProject/ECS/service/ECSService.java
@@ -2,18 +2,27 @@ package carProject.ECS.service;
 
 import carProject.CCS.models.UserCredentials;
 import com.example.Jobs.model.Job;
+import com.firstexample.demo.dto.ListOfCarsDTO;
+import com.firstexample.demo.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ECSService {
+
+    @Autowired
+    private KafkaTemplate<String, Car> kafkaTemplate;
+    private static final String TOPIC = "car_consumer";
 
     @Value("${ccs.server.address}")
     private String ccsAddress;
@@ -21,7 +30,8 @@ public class ECSService {
     @KafkaListener(topics = "kafka_job")
     public void getFromKafka(Job job) throws UnsupportedEncodingException {
         UserCredentials uc = getUserCredentials(job);
-        List<Object> cars = getCars(uc,job);
+        List<Car> cars = getCars(uc,job);
+        sendCars(cars);
     }
 
     private UserCredentials getUserCredentials(Job job) {
@@ -41,11 +51,17 @@ public class ECSService {
         return result;
     }
 
-    private List<Object> getCars(UserCredentials uc,Job job) throws UnsupportedEncodingException {
+    private List<Car> getCars(UserCredentials uc, Job job) throws UnsupportedEncodingException {
         final String uri = uc.getApi_url() + makeQueryFromJob(job);
         System.out.println(uri);
         RestTemplate restTemplate = new RestTemplate();
-        return (List<Object>) restTemplate.getForObject(uri, Object.class);
+        return (List<Car>) restTemplate.getForObject(uri, ListOfCarsDTO.class).getCars();
+    }
+
+    private void sendCars(List<Car> cars){
+        for(Car car:cars){
+            kafkaTemplate.send(TOPIC,car);
+        }
     }
 
 }
